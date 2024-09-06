ed591c218d98235e2e6f1d20f92d6a0ac8a4370a
Phi Thach <jokivadet@gmail.com>
2024-09-06 22:07:47 +0700

    update timer countdown

---
+++ fake-backend/auth.js
@@ -0,0 +1,11 @@
+const fakeResponse = {
+  status: 200, // Replace with the desired status code (e.g., 404, 500)
+  data: {
+    // Your desired fake data structure
+    message: "This is a fake response",
+    data: [1, 2, 3],
+  },
+  headers: {
+    "Content-Type": "application/json", // Set the appropriate content type
+  },
+};
---
+++ js/function.js
@@ -0,0 +1,23 @@
+function countdown30Minutes() {
+  const timer = document.querySelector(".timer");
+
+  const endDate = new Date().getTime() + 30 * 60 * 1000;
+
+  const x = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = endDate - now;
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    timer.innerHTML =
+      minutes + ":" + ((seconds < 10 && "0" + seconds) || seconds);
+
+    if (distance < 0) {
+      clearInterval(x);
+      demo.innerHTML = "Hết giờ!";
+    }
+  }, 1000);
+}
+
+countdown30Minutes();
