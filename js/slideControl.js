07427611c14d41a7f69a51c7bc4e1fb1fce41199
lephithach <jokivadet@gmail.com>
2025-01-18 10:43:32 +0700

    update

---
+++ js/slideControl.js
@@ -0,0 +1,60 @@
+// Slider main
+$(".slider-container .slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  dots: true,
+  autoplaySpeed: 1200,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: true,
+      },
+    },
+  ],
+});
+
+// Glallery slider
+$(".gallery-slider .slider").slick({
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 1200,
+  infinite: true,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        arrows: false,
+      },
+    },
+  ],
+});
+
+//       $('.gallery-slider .slider').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: '.slider-nav'
+// });
+// $('.slider-nav').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: '.slider-for',
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true
+// });
